22230a66c8f861643e54df16c247834e99095d0c
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-09-11 11:38:26 +0400

    Add first EF Core app

---
+++ EF Core App/EF Core App/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_Core_App
+{
+    class ApplicationContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=usersdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ EF Core App/EF Core App/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_Core_App
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
