61c31c3657c97dc3c7c900fab6781432111262f9
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-09-14 12:45:51 +0400

    add CRUD

--- EF Core App/EF Core App/Program.cs
+++ EF Core App/EF Core App/Program.cs
@@ -27,6 +27,52 @@ namespace EF_Core_App
                     Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
                 }
             }
+
+            // Редактирование
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем первый объект
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    user.Name = "Bob";
+                    user.Age = 44;
+                    //обновляем объект
+                    //db.Users.Update(user);
+                    db.SaveChanges();
+                }
+                // выводим данные после обновления
+                Console.WriteLine("\nДанные после редактирования:");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+            // Удаление
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // получаем первый объект
+                User user = db.Users.FirstOrDefault();
+                if (user != null)
+                {
+                    //удаляем объект
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                    //Если необходимо удалить сразу несколько объектов, то можно использовать метод RemoveRange():
+                    //User user1 = db.Users.FirstOrDefault();
+                    //User user2 = db.Users.LastOrDefault();
+                    //db.Users.RemoveRange(user1, user2);
+                }
+                // выводим данные после обновления
+                Console.WriteLine("\nДанные после удаления:");
+                var users = db.Users.ToList();
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
             Console.Read();
         }
     }
---
+++ EF Core App/EF Core DB/Program.cs
@@ -0,0 +1,27 @@
+﻿//Для работы с существующей БД MS SQL Server нам надо добавить три пакета:
+//Microsoft.EntityFrameworkCore.SqlServer
+//Microsoft.EntityFrameworkCore.Tools
+//Microsoft.EntityFrameworkCore.SqlServer.Design
+
+/*
+Пакет "Microsoft.EntityFrameworkCore.SqlServer" представляет функциональность Entity Framework для работы с MS SQL Server, 
+а два других пакета - "Microsoft.EntityFrameworkCore.Tools" и "Microsoft.EntityFrameworkCore.SqlServer.Design" понадобятся 
+нам в дальнейшем для создания классов по базе данных, то есть reverse engineering.
+*/
+
+/*
+ Далее в Package Manager Console выполним следующую команду:
+Scaffold-DbContext "Server=(localdb)\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;" Microsoft.Entit
+ */
+using System;
+
+namespace EF_Core_DB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
